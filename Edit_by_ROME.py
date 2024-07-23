8341da4990e403fa9cf5a3a509c73aba858277ea
Mongjin <30318269+Mongjin@users.noreply.github.com>
2024-07-23 13:08:19 +0900

    Add Edit by ROME

--- Edit_by_ROME.py
+++ Edit_by_ROME.py
@@ -44,7 +44,7 @@ print(type(edited_model))
 
 ### Reliability Test ###
 
-tokenizer = LlamaTokenizer.from_pretrained('meta-llama/Meta-Llama-3-8B-Instruct', cache_dir='./hugging_cache')
+tokenizer = LlamaTokenizer.from_pretrained('meta-llama/Meta-Llama-3-8B-Instruct')
 tokenizer.pad_token_id = tokenizer.eos_token_id
 tokenizer.padding_side='left'
 
@@ -53,7 +53,7 @@ correct_prompts = ['Who was the designer of Lahti Town Hall?',
                 'What city did Marl Young live when he died?']
 
 
-model = LlamaForCausalLM.from_pretrained('meta-llama/Meta-Llama-3-8B-Instruct', cache_dir='./hugging_cache').to('cuda')
+model = LlamaForCausalLM.from_pretrained('meta-llama/Meta-Llama-3-8B-Instruct')
 batch = tokenizer(correct_prompts, return_tensors='pt', padding=True, max_length=30)
 
 pre_edit_outputs = model.generate(
